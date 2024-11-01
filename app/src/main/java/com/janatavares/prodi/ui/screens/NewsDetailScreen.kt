0543ea402d3e110c2c2919bdf7eed1831bd71d61
Janaina Tavares <52297249+jana-dev@users.noreply.github.com>
2024-11-01 15:47:24 -0300

    Ajuste na tela de detalhes da noticia

--- app/src/main/java/com/janatavares/prodi/ui/screens/NewsDetailScreen.kt
+++ app/src/main/java/com/janatavares/prodi/ui/screens/NewsDetailScreen.kt
@@ -2,6 +2,7 @@ package com.janatavares.prodi.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -14,41 +15,43 @@ import com.janatavares.prodi.data.model.NewsItem
 
 @Composable
 fun NewsDetailScreen(newsItem: NewsItem, onBack: () -> Unit) {
-    Column(
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        // Botão de voltar
-        TextButton(onClick = { onBack() }) {
-            Text("Voltar", color = MaterialTheme.colorScheme.primary)
+        item {
+            // Botão de voltar
+            TextButton(onClick = { onBack() }) {
+                Text("Voltar", color = MaterialTheme.colorScheme.primary)
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Exibe a imagem da notícia
+            Image(
+                painter = painterResource(id = newsItem.imageResId),
+                contentDescription = "Imagem da notícia",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .padding(bottom = 16.dp)
+            )
+
+            // Título da notícia
+            Text(
+                text = newsItem.title,
+                style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.Bold),
+                color = MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+
+            // Descrição completa da notícia
+            Text(
+                text = newsItem.fullText,
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onSurface
+            )
         }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        // Exibe a imagem da notícia
-        Image(
-            painter = painterResource(id = newsItem.imageResId),
-            contentDescription = "Imagem da notícia",
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp)
-                .padding(bottom = 16.dp)
-        )
-
-        // Título da notícia
-        Text(
-            text = newsItem.title,
-            style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.Bold),
-            color = MaterialTheme.colorScheme.onSurface,
-            modifier = Modifier.padding(bottom = 8.dp)
-        )
-
-        // Descrição completa da notícia
-        Text(
-            text = newsItem.fullText,
-            style = MaterialTheme.typography.bodyLarge,
-            color = MaterialTheme.colorScheme.onSurface
-        )
     }
 }
